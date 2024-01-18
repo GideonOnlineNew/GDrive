d303acfc92681db2a0bd3fc40e5c0a9ff0db4549
GideonOnlineNew <156960908+GideonOnlineNew@users.noreply.github.com>
2024-01-18 21:19:59 +0200

    Update background.js

--- Scripts/background.js
+++ Scripts/background.js
@@ -1,3 +1,5 @@
+import * as THREE from './path/to/three.module.js';
+import { GLTFLoader } from './path/to/GLTFLoader.js';
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
